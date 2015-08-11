8f8411a455e3ab02a103278917ffed29975b0af7
ttriggs <tyler.triggs@gmail.com>
2015-08-11 15:44:13 -0500

    test board #new accepts state and turn arguements

--- spec/lib/board_spec.rb
+++ spec/lib/board_spec.rb
@@ -2,13 +2,23 @@ require 'board'
 
 describe Board do
   let(:new_board) { Board.new }
+  let(:used_board) { Board.new(%w(x - - - - - - - -),'o') }
 
-  describe 'new' do
-    it 'sets #state to nine dashes' do
+  describe '.new(state,turn)' do
+    it 'defaults #state to nine dashes' do
       expect(new_board.state).to eq(%w(-) * Board::SIZE)
     end
-    it 'sets #turn == x' do
+    it 'defaults #turn == x' do
       expect(new_board.turn).to eq('x')
     end
+    it 'sets @turn if passed in' do
+      expect(used_board.turn).to eq('o')
+    end
+    it 'sets @state if passed board' do
+      expect(used_board.state).to eq(%w(x - - - - - - - -)) 
+    end
+  end
+
+  describe '#move(index)' do
   end
 end
