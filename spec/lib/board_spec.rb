f07815e8109e12f5d8c825b638cbe219125be514
ttriggs <tyler.triggs@gmail.com>
2015-08-11 16:21:51 -0500

    test #move(index) adds token for current player

--- spec/lib/board_spec.rb
+++ spec/lib/board_spec.rb
@@ -1,16 +1,18 @@
 require 'board'
 
 describe Board do
-  let(:new_board) { Board.new }
-  let(:used_board) { Board.new(%w(x - - - - - - - -),'o') }
-
-  describe '.new(state,turn)' do
+  context '.new(state,turn) defaults' do
     it 'defaults #state to nine dashes' do
-      expect(new_board.state).to eq(%w(-) * Board::SIZE)
+      expect(Board.new.state).to eq(%w(-) * Board::SIZE)
     end
     it 'defaults #turn == x' do
-      expect(new_board.turn).to eq('x')
+      expect(Board.new.turn).to eq('x')
     end
+  end
+
+  context '.new(state,turn) input arguements' do
+    let(:used_board) { Board.new(%w(x - - - - - - - -),'o') }
+
     it 'sets @turn if passed in' do
       expect(used_board.turn).to eq('o')
     end
@@ -19,6 +21,12 @@ describe Board do
     end
   end
 
-  describe '#move(index)' do
+  context '#move(index)' do
+    let(:board) { Board.new.move(0) }
+    it 'makes move for current player (x)' do
+      expect(board.state).to eq(%w(x - - - - - - - -))
+      expect(board.turn).to eq("o")
+    end
+    
   end
 end
