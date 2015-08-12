f4f074210dfaa9eb119c22b58c250ffa39b8da10
ttriggs <tyler.triggs@gmail.com>
2015-08-12 13:06:40 -0500

    test #minimax for win, near-win cases, and tie

--- spec/lib/board_spec.rb
+++ spec/lib/board_spec.rb
@@ -25,7 +25,7 @@ describe Board do
     let(:board) { Board.new.move(0) }
     it 'makes move for current player (x)' do
       expect(board.state).to eq(%w(x - - - - - - - -))
-      expect(board.turn).to eq("o")
+      expect(board.turn).to eq('o')
     end
   end
 
@@ -38,38 +38,79 @@ describe Board do
 
   context '#win?' do
     it 'finds no winner for fresh board' do
-      expect(Board.new.win?("x")).to be_falsey
-      expect(Board.new.win?("o")).to be_falsey
+      expect(Board.new.win?('x')).to be_falsey
+      expect(Board.new.win?('o')).to be_falsey
     end
+
     it 'finds winner in rows' do
       expect(Board.new(%w(x x x
                           - - -
-                          - - -)).win?("x")).to be_truthy
+                          - - -)).win?('x')).to be_truthy
       expect(Board.new(%w(- - -
                           x x x
-                          - - -)).win?("x")).to be_truthy
+                          - - -)).win?('x')).to be_truthy
       expect(Board.new(%w(- - -
                           - - -
-                          x x x)).win?("x")).to be_truthy
+                          x x x)).win?('x')).to be_truthy
     end
+
     it 'finds winner in cols' do
       expect(Board.new(%w(o - -
                           o - -
-                          o - -)).win?("o")).to be_truthy
+                          o - -)).win?('o')).to be_truthy
       expect(Board.new(%w(- o -
                           - o -
-                          - o -)).win?("o")).to be_truthy
+                          - o -)).win?('o')).to be_truthy
       expect(Board.new(%w(- - o
                           - - o
-                          - - o)).win?("o")).to be_truthy
+                          - - o)).win?('o')).to be_truthy
     end
+
     it 'finds winner in diagonals' do
       expect(Board.new(%w(o - -
                           - o -
-                          - - o)).win?("o")).to be_truthy
+                          - - o)).win?('o')).to be_truthy
       expect(Board.new(%w(- - o
                           - o -
-                          o - -)).win?("o")).to be_truthy
+                          o - -)).win?('o')).to be_truthy
+    end
+  end
+
+  context '#minimax(depth)' do
+    it 'returns 100 if win?(x)' do
+      expect(Board.new(%w(x x x - - - - - -)).minimax).to eq(100)
+    end
+    it 'returns -100 if win?(o)' do
+      expect(Board.new(%w(o o o - - - - - -)).minimax).to eq(-100)
+    end
+    it 'returns 0 if no remaining possible_moves (tie game)' do
+      expect(Board.new(%w(o x o
+                          o o x
+                          x o x)).minimax).to eq(0)
+    end
+    it 'returns 99 if x is 1 move from win' do
+      expect(Board.new(%w(x x -
+                          - - -
+                          - - -),"x").minimax).to eq(99)
+    end
+    it 'returns -99 if o is 1 move from win' do
+      expect(Board.new(%w(o o -
+                          - - -
+                          - - -),"o").minimax).to eq(-99)
+    end
+  end
+  context '#best_move' do
+    it 'returns best move for current player' do
+      expect(Board.new(%w(o o -
+                          - - -
+                          - - -),"o").best_move).to eq(2)
+      expect(Board.new(%w(o - -
+                          o - -
+                          - - -),"o").best_move).to eq(6)
+      expect(Board.new(%w(- - -
+                          - o -
+                          - - o),"o").best_move).to eq(0)
     end
   end
 end
+
